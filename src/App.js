50fc40fa93577cbd64fe77c94f4a998ddad23a36
duchieu <duchieu.ctk41@gmail.com>
2021-01-06 15:50:09 +0700

    [1.0.0] them cart-list, chia 4 cot san pham.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
-import logo from "./logo.svg";
+import { CardList } from "./components/card-list/card-list.components";
+
 import "./App.css";
 
 class App extends Component {
@@ -9,17 +10,19 @@ class App extends Component {
     this.state = { monsters: [] };
   }
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users/')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }))
+    fetch("https://jsonplaceholder.typicode.com/users/")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}> {monster.name} </h1>
-        ))}
+        <CardList nam="loz">
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}> {monster.name} </h1>
+          ))}
+        </CardList>
       </div>
     );
   }
