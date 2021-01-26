ea7a8162c959aaeaf93f7509af768c72e70ece42
duchieu <duchieu.ctk41@gmail.com>
2021-01-26 12:16:55 +0700

    fig bug 2

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <div>
+    ddmm
     <App />
-  </React.StrictMode>,
+  </div>,
   document.getElementById('root')
 );
 
